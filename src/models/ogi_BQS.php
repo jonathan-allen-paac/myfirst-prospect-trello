fe2c27ef20302f0ca030eb6af772e6089be289ef
jonathan-allen-paac <jonathan.allen@paac-it.com>
2025-10-20 22:45:39 +0100

    Update ogi_BQS.php

--- src/models/ogi_BQS.php
+++ src/models/ogi_BQS.php
@@ -15,11 +15,20 @@ $OGIQuery = "SELECT
         Freetext2,
         P.[PolicyRef@],
         P.[ClientRef@],
-        B.LastUpdatedTime
+        B.LastUpdatedTime,
+		V.Model,
+		M.VTDescription AS Make,
+		O.VTDescription AS Occupation,
+		T.VTDescription AS LicenseType
     FROM
-        icp_Daprospect C INNER JOIN
-        icp_Dapolicy P ON C.[Branch@] = P.[Branch@] AND C.[ClientRef@] = P.[ClientRef@] INNER JOIN
-        icp_DP_BQS B ON B.[Branch@] = C.[Branch@] AND B.[PolicyRef@] = P.[PolicyRef@]";
+        icp_Daprospect C
+		INNER JOIN icp_Dapolicy P ON C.[Branch@] = P.[Branch@] AND C.[ClientRef@] = P.[ClientRef@]
+		INNER JOIN icp_DP_BQS B ON B.[Branch@] = C.[Branch@] AND B.[PolicyRef@] = P.[PolicyRef@]
+		LEFT JOIN icp_DP_CF V on V.[PolicyRef@] = P.[PolicyRef@]
+		LEFT JOIN icp_QR26_VT M ON M.VTId = V.Make_QR26_VTId
+		LEFT JOIN icp_DP_PC L ON L.[PolicyRef@] = P.[PolicyRef@]
+		LEFT JOIN icp_QR35_VT O ON O.VTId = L.Ft_occup_QR35_VTId
+		LEFT JOIN icp_QR05_VT T ON T.VTId = L.Licence_QR05_VTId";
 
 //OGI CHKD Sync
 function ogi_sync_BQS($day) {
@@ -65,7 +74,11 @@ function ogi_sync_BQS($day) {
                 'Addr4' => $row['Addr4'],
                 'Pcode' => $row['Pcode'],
                 'Dob' => $dob,
-                'Freetext2' => $row['Freetext2']
+                'Freetext2' => $row['Freetext2'],
+                'Model' => $row['Model'],
+                'Make' => $row['Make'],
+                'Occupation' => $row['Occupation'],
+                'LicenseType' => $row['LicenseType']
             ];
         }
     }
@@ -91,11 +104,16 @@ function ogi_sync_BQS($day) {
             $Pcode = $row['Pcode'];
             $Dob = $row['Dob'];
             $Freetext2 = $row['Freetext2'];
+            $Model = $row['Model'];
+            $Make = $row['Make'];
+            $Occupation = $row['Occupation'];
+            $LicenseType = $row['LicenseType'];
             
             mysqlInsert("INSERT INTO `myfirst-prospect-trello` (`B@`, `Key@`, `LastUpdatedTime`, `ClientRef@`, `PolicyRef@`,`Synced`,
-            `#Name`,`Email`,`Tel`,`Addr1`,`Addr2`,`Addr3`,`Addr4`,`Pcode`,`Dob`,`Freetext2`)
+            `#Name`,`Email`,`Tel`,`Addr1`,`Addr2`,`Addr3`,`Addr4`,`Pcode`,`Dob`,`Freetext2`,Model,Make,Occupation,LicenseType)
             VALUES ('$B', '$Key', '$LastUpdatedTime', '$ClientRef', '$PolicyRef', NOW(),
-            '$Name','$Email','$Tel','$Addr1','$Addr2','$Addr3','$Addr4','$Pcode','$Dob','$Freetext2')");
+            '$Name','$Email','$Tel','$Addr1','$Addr2','$Addr3','$Addr4','$Pcode','$Dob','$Freetext2',
+            '$Model','$Make','$Occupation','$LicenseType')");
         }
         mysqlInsert("INSERT INTO `run_log` (`function`) VALUES ('".__FUNCTION__."')");
         if(isset($msc)) {$msc2=microtime(true)-$msc; echo "<span>Server processed in".round($msc2,5)." seconds</span>";}
